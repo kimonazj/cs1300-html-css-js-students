2d85a08f292ac5e352d01fcb11ab17aea2ccefb3
Ji (Anna) Zhang <65613966+kimonazj@users.noreply.github.com>
2022-09-11 17:55:08 -0400

    finish todo

--- app.js
+++ app.js
@@ -26,14 +26,21 @@ const updatePage = async () => {
 
   // Make API request and get an array of fruit objects
   const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
 
   // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15",
+  const highSugar = fruitsArray.filter(fruit => fruit.nutritions.sugar > 15);
 
   // TODO: Create a new HTML element to display your data
+  const newElement = document.createElement('div');
+  const htmlline = highSugar.map(x => "<p>"+x.name + "'s sugar level: "+x.nutritions.sugar+"<p>");
+  newElement.innerHTML = htmlline.join('');
 
   // TODO: Append your new element to the page
+  const existingElement = document.getElementById('cs1300-gallery');
+  existingElement.append(newElement);
+
+  document.getElementById("btn").remove();
 
 }
 
